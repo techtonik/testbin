fbb8aec3650b45f690c4febfd621fe5d6892b14a
anatoly techtonik <techtonik@gmail.com>
2016-08-18 21:44:01 +0300

    Add encoding-problem-cp1252.py bug

---
+++ python/encoding-problem-cp1252.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: cp1252 -*-
+
+
+"""
+There is nothing in this file, except that it is more
+than 50 lines long. Running it with Python 3.5.2 on
+Windows gives the following error:
+
+    >python encoding-problem-cp1252.py
+      File "encoding-problem-cp1252.py", line 2
+    SyntaxError: encoding problem: cp1252
+
+    >python
+    Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+    Type "help", "copyright", "credits" or "license" for more information.
+
+If you remove any lines from this file, it will
+execute successfully.
+"""
+
+
+
+def restore(dump):
+  """
+
+
+
+
+
+
+
+
+
+
+
+
+
+  """
+  return
+
+
+
+def main():
+  print('ok')
+
+
+
+if __name__ == '__main__':
+  main()
